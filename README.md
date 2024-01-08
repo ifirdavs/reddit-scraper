8fb15171b1e41f0f4894218e4c24664bd145370c
ifirdavs <fismoilov20@gmail.com>
2024-01-09 01:51:48 +0500

    Major Update
    - Updated scraper.py
    - Added some Code Explanation to README.md
    - Added some changes to .gitignore

--- README.md
+++ README.md
@@ -23,3 +23,64 @@ soup = BeautifulSoup(response.content, 'html.parser')       # respone.content in
 Remember, you should replace the User-Agent string with the one that represents your actual browser.
 
 You can find your User-Agent by visiting a website like <https://www.whatismybrowser.com/detect/what-is-my-user-agent>. This website will display your User-Agent string, which you can then copy and use in your Python script.
+
+
+## Scrapy
+
+<https://docs.scrapy.org/en/latest/intro/tutorial.html>
+<https://phantombuster.com/1146996741091941/phantoms/5545416691289264/console>
+<https://www.youtube.com/watch?v=d4iz2NrZVRg>
+<https://scrapeops.io/app/dashboard>
+
+```shell
+pip install scrapy scrapeops-scrapy scrapeops-scrapy-proxy-sdk
+```
+
+--
+
+--
+
+--
+
+## How To Run
+
+```shell
+scraper.py -p <number_of_posts> -c <category> -t <timeframe>
+```
+
+### Options:
+
+ `-p` or `--posts` – Number of posts to scrape. Default is 10.
+
+ `-c` or `--category` – Category of posts to scrape (`Best`, `Hot`, `New`, `Top`, `Rising`). Default is `Top`.
+
+ `-t` or `--timeframe` – Timeframe (if acceptable) of posts to scrape. Default is `month`.
+
+
+## Code Explanation
+
+Lines 27-28: Empty a file named `report.txt` if it exists. This file will be used to store the scraped data.
+
+```python
+with open('report.txt', 'w'):
+    pass
+```
+
+---
+Lines 33-34: Update the with the new url to scrape.
+
+```python
+url = soup.find('faceplate-partial', {'slot': 'load-after'}).get('src')
+url = f'https://www.reddit.com{url}'
+```
+
+---
+Lines 72-76: If file with the same name as `file_name` doesn't exist, we can safely create final report file. Otherwise, we need to delete the existing file and create a new one.
+
+```python
+if not os.path.exists(file_name):
+    os.rename('report.txt', file_name)
+else:
+    os.remove(file_name)
+    os.rename('report.txt', file_name)
+```
--- scraper.py
+++ scraper.py
@@ -9,25 +9,30 @@ logging.basicConfig(filename='reddit.log', level=logging.INFO)
 
 parser = argparse.ArgumentParser(description='Scrape Reddit')
 
-parser.add_argument('--posts', type=int, default=100, help='Number of posts')
-parser.add_argument('--category', type=str, default="top", help='Category')
+parser.add_argument('-p', '--posts', type=int, default=100, help='Number of posts')
+parser.add_argument('-c', '--category', type=str, default="top", help='Category')
 parser.add_argument('--period', type=str, default="month", help='Period')
 
 args = parser.parse_args()
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
 
-def distances():
-    for i in range(4, 2905, 29):
-        yield i
-
-counter = 0
+post_counter = 0
+error_counter = 0
 
 break_flag = False
-for d in distances():
-    url = f'https://www.reddit.com/svc/shreddit/feeds/popular-feed?after=dDNfMTdqdmc1Mw%3D%3D&distance={d}&sort={args.category}&t={args.period}'
+
+url = f'https://www.reddit.com/svc/shreddit/feeds/popular-feed?sort={args.category}&t={args.period}'
+
+with open('report.txt', 'w'):
+    pass
+
+while True:
     soup = BeautifulSoup(requests.get(url).content, 'html.parser')
     posts = soup.find_all('shreddit-post')
+    url = soup.find('faceplate-partial', {'slot': 'load-after'}).get('src')
+    url = f'https://www.reddit.com{url}'
+    
     for post in posts:
         try:
             post_id = uuid.uuid1().hex
@@ -46,15 +51,17 @@ for d in distances():
             post_date = post.get('created-timestamp')
             num_comments = post.get('comment-count')
             num_votes = post.get('score')
+
             with open('report.txt', 'a') as f:
                 f.write('{};{};{};{};{};{};{};{};{};{};{}\n'.format(post_id, post_url, username, user_karma, user_cake_day, post_karma, comment_karma, post_date, num_comments, num_votes, post_category))
-            counter += 1
-            logging.info('Post {} successfully scraped'.format(counter))
+            post_counter += 1
+            logging.info('Post {} successfully scraped'.format(post_counter))
 
         except Exception as e:
             logging.error(f'{datetime.datetime.now()} Error scraping post {user_page_url}: {e}')
-        
-        if counter == args.posts:
+            error_counter += 1
+
+        if post_counter == args.posts or error_counter == 10:
             break_flag = True
             break
     if break_flag:
